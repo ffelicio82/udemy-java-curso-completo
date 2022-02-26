1f501e076023c5a257b10f34ff739681ec18ed61
Fernando <fernando_felicio82@yahoo.com.br>
2022-02-25 23:31:43 -0300

    Estudo básico
    - Uso da classe Scanner;
    - Criação de uma classe para calcular a média.

---
+++ basico/Classe.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Classe {
+    public static void main(String[] args) {
+        Media media = new Media();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        media.setX(scanner.nextDouble());
+
+        System.out.println("Digite o segundo número: ");
+        media.setY(scanner.nextDouble());
+
+        System.out.println("Media (objeto): " + media);
+        System.out.println("A média é: " + media.calcular());
+
+        scanner.close();
+    }
+}
