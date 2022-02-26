53e460db1739b99b096456ab02c30f3d00dfa51c
Fernando <fernando_felicio82@yahoo.com.br>
2022-02-25 23:31:43 -0300

    Estudo básico
    - Uso da classe Scanner;
    - Criação de uma classe para calcular a média;
    - Inclusão do package.

---
+++ basico/Media.java
@@ -0,0 +1,22 @@
+package basico;
+public class Media {
+    private double x;
+    private double y;
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double calcular() {
+        return (x + y) / 2.0;
+    }
+
+    @Override
+    public String toString() {
+        return "{Media: {'x' : " + this.x + ", 'y' : " + this.y + "}}";
+    }
+}
